8fd1114b8273ad2f992f4ae997769bec35757a8a
Win Win Aye <141402516+WinWinAye200199@users.noreply.github.com>
2026-09-12 16:59:03 +1200

    Initial Spring Boot backend setup with database configuration

---
+++ backend/grocery-ordering-api/src/main/java/com/comp713/grocery_ordering_api/GroceryOrderingApiApplication.java
@@ -0,0 +1,13 @@
+package com.comp713.grocery_ordering_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroceryOrderingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroceryOrderingApiApplication.class, args);
+	}
+
+}
---
+++ backend/grocery-ordering-api/src/test/java/com/comp713/grocery_ordering_api/GroceryOrderingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comp713.grocery_ordering_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroceryOrderingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
